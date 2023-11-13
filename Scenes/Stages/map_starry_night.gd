3ba69b9e6aa12c168e48afcc03a939a2d19c4150
tschnittger <toni_mathias.schnittger@fh-bielefeld.de>
2023-11-13 17:24:04 +0100

    Skript for new map

---
+++ Scenes/Stages/map_starry_night.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	get_node("Player").init()
+	get_node("Player").get_node("Camera2D").get_node("Time").timer(true)
